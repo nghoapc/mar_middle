5115d65d15ea942f55831fffc346f22798175535
HoaNg <nghoapc@gmail.com>
2019-08-21 00:43:56 +0700

    add call staging

--- app/services/track_sale_service.rb
+++ app/services/track_sale_service.rb
@@ -1,7 +1,9 @@
 class TrackSaleService < BaseService
 
 	def push_sale(sale_params)
-		url =  "http://demo.maralytics.dfksoft.com/api/v1/track_sales"
-		RestClient.post( url, sale_params)
+		urls =  ["http://demo.maralytics.dfksoft.com/api/v1/track_sales", "http://staging.maralytics.dfksoft.com/api/v1/track_sales"]
+		urls.each do |url|
+			RestClient.post( url, sale_params)
+		end
 	end
 end
